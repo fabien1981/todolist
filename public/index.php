e9fca2ebb7270341c2afeda05ffb57a92bdfdba2
fabien1981 <166442156+fabien1981@users.noreply.github.com>
2025-01-28 18:56:26 +0100

    Ajout de styles personnalisés pour l'interface utilisateur

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@
     <title>To-Do List</title>
     <!-- Lien vers Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
 <div class="container mt-5">
